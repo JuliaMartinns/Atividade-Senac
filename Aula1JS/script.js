e2adee7ff6b0c6e5374f179fa415383c3d924a7a
Júlia <pjuliammr@gmail.com>
2026-04-28 16:49:44 -0300

    Add files via upload

---
+++ Aula1JS/script.js
@@ -0,0 +1,133 @@
+function PATRICK() {
+
+    alert("Olá, mundo!");
+    let nome = ("Julião");
+    alert(nome);}
+
+function LULA_MOLUSCO() {
+    let numero1 = 10;
+    let numero2 = 20;
+    
+    idade = numero1 + numero2;
+    alert(idade);
+
+}
+
+function esponja(){
+    let texto = document.getElementById("bob");
+    texto.textContent = "Estamos capitão!";
+}
+
+function abacaxi(){ 
+    let cor = document.getElementById("abacaxi"); 
+    cor.style.backgroundColor = "yellow";
+}
+
+function sandy(){
+    document.getElementById("botton").textContent ="EU NÃO OUVI DIREITO!";
+}
+
+function sr_ciriguejo(){
+    let texto = document.getElementById("cirigueijo");
+    texto.textContent = "";
+}
+
+
+function funcao(){
+    let par = document.getElementById("paragrafo");
+    let cax = document.getElementById("caixa");
+    let escrita = par.textContent;
+    cax.value = escrita;
+}
+
+function soma(){
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    let resultado = num1 + num2;
+    let local = document.getElementById("resultado");
+    local.textContent = resultado;
+}
+
+function fundo(){
+   document.body.style.backgroundColor = "pink";
+}
+
+function tamanho(){
+    let gestor = document.getElementById("alt").style.fontSize = "30px";
+}
+
+function CorTop(){
+    let topissimo = document.getElementById("CorTop").style.color = "purple";
+}
+
+function DaviBrito(){
+    let magica = document.getElementById("abracadabra").style.backgroundColor="magenta";
+    let magica2 = document.getElementById("abracadabra").style.color="white";
+
+}
+
+function DaviBrito2(){
+    let feitiço = document.getElementById("abracadabra2").style.backgroundColor="purple";
+    let feitiço2 = document.getElementById("abracadabra2").style.color = "white";
+    let feitiço3 = document.getElementById("abracadabra2").style.fontSize = "50px";
+}
+
+
+function DaviBrito3(){
+    let Barbie = document.getElementById("Barbie");
+    Barbie.textContent = "Namorado, fica ligado!";
+    Barbie.style.backgroundColor = "pink";
+    Barbie.style.fontSize = "40px";
+}
+
+function Virginia(){
+    let escrita = document.getElementById("Juliao").value;
+    let lugar1 = document.getElementById("CorTop");
+    let lugar2 = document.getElementById("abracadabra");
+    lugar1.textContent = escrita;
+    lugar2.textContent = escrita;
+}
+
+function AlterarTexto(){
+    let texto = document.getElementById("Alterado").textContent = "O Raphael é um gestor muito top!";
+}
+
+function AlterarCor(){
+    let Cor = document.getElementById("Alterado").style.color = "red";
+}
+
+function AlterarTamanho(){
+    let Tamanho = document.getElementById("Alterado").style.fontSize = "30px";
+}
+
+
+function VIRGINIA(){
+    let fundo = document.getElementById("ViniJR").style.backgroundColor = "green";
+    let fundo2 = document.getElementById("ZéFelipe").style.backgroundColor = "orange";
+}
+
+function BobbieGoods(){
+    let escrita = document.getElementById("Mãe").value;
+    let lugar1 = document.getElementById("parágrafo");
+    lugar1.textContent = escrita;
+
+    let cor = document.getElementById("parágrafo").style.backgroundColor = "blue";
+}
+
+function botão1(){
+    let numero1 = document.getElementById("numero1").value;
+    let numero2 = document.getElementById("numero2").value;
+    let ttl = Number(numero1) + Number(numero2);
+    document.getElementById("resultado2").textContent = ttl;
+    
+}
+
+function botão2() {
+    document.getElementById("resultado2").textContent = "";
+}
+
+function PagCompleta(){
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+    document.body.style.fontSize = "20px";
+}
